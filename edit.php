24168e235e8d7051b350be7c268213e16a82e689
Rene Olskjaer <rene@olskjaer.dk>
2020-11-06 11:07:51 +0100

    change

---
+++ edit.php
@@ -0,0 +1,106 @@
+<?php
+header('Content-type:text/html; charset=utf-8');
+require_once('setup/setup.php');
+?>
+<!DOCTYPE html>
+<html lang=da>
+<head>
+ <title>ZenseHome</title>
+<meta name="viewport" content="width=device-width, initial-scale=0.6">
+
+<style>
+
+
+<?php include 'CSS/main.css'; ?>
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+</head>
+<body style="background-color:lightgrey;">
+<div class=fadeMe id='progress' style='display:none'>
+   <div class=container>
+       <img class=img src='images/ajax-loader.png' alt='loader'>
+   </div>
+</div>
+
+<?php
+
+function edit($ip, $port, $login, $id)
+{
+    $u = array();
+    $conn = new mysqli($GLOBALS['mysqlserver'], $GLOBALS['user'], $GLOBALS['password']);
+    mysqli_set_charset($conn, "utf8");
+    $query = 'SELECT * from zense.box_' . $login . ' where id='.$id.';';
+    $result = mysqli_query($conn, $query);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $name = $row["name"];
+            $room = $row["room"];
+            $floor = $row["floor"];
+            $dim = $row["dim"];
+        }
+    }
+    $conn->close();
+    echo '<form action="edit.php?id='.$id.'" method="post">';
+    echo '<table style="width:100%">';
+    echo '<tr style="background-color:#3EA5C4;">';
+    echo '<td style="border:none;text-align: left"><a href="/"><img src="images/undo.png" alt="undo" id="undo" width="60" height="60"></a></td>';
+    echo '<td style="border:none;text-align: right"><input type="image" src="images/save.png" alt="Submit" name="save" width="60" height="60"></td>';
+    echo '<tr>';
+    echo '<th style="text-align: left; font-size:xx-large;">Floor</th>';
+    echo  '<th style="text-align: right; width:20%"> <input type="text" name="floor" value='.$floor.' id="inputForm1"></th>';
+    echo '</tr>';
+   
+    echo '<tr>';
+    echo '<th style="text-align: left; font-size:xx-large;">Room</th>';
+    echo  '<th style="text-align: right; width:20%"> <input type="text" name="room" value='.$room.' id="inputForm1"></th>';
+    echo '</tr>';
+    echo '<tr>';
+    echo '<th style="text-align: left; font-size:xx-large;">Name</th>';
+    echo  '<th style="text-align: right; width:20%"> <input type="text" name="name" value='.$name.' id="inputForm1"></th>';
+    echo '</tr>';
+
+    echo '<tr>';
+    echo '<th style="text-align: left; font-size:xx-large;">Dimmable</th>';
+    echo  '<th style="text-align: right; width:20%"> <label class="switch"> <input name="dim"';
+    if ($dim>0) {
+        echo  ' type="checkbox" checked> <span class="slider round"></span></label></th>';
+    } else {
+        echo  ' type="checkbox" > <span class="slider round"></span></label></th>';
+    }
+    echo '</tr>';
+
+     echo '</table>';
+    echo '</form>';
+ 
+  
+}
+
+   $login= htmlspecialchars($_COOKIE['BOXID']);
+   $ip= htmlspecialchars($_COOKIE['IP']);
+   $port= htmlspecialchars($_COOKIE['PORT']);
+   $oldstate = htmlspecialchars($_COOKIE['STATE']);
+
+if (isset($_POST['save_x'])) {
+    $conn = new mysqli($GLOBALS['mysqlserver'], $GLOBALS['user'], $GLOBALS['password']);
+    mysqli_set_charset($conn, "utf8");
+    $dim=0;
+    if($_POST["dim"]=="on") $dim = 1;
+    $name='\''.$_POST["name"].'\'';
+    $room='\''.$_POST["room"].'\'';
+    $floor='\''.$_POST["floor"].'\'';
+    $query = 'UPDATE zense.box_' . $login . ' SET name="'.$name.'",room="'.$room.'", floor="'.$floor.'", dim="'.$dim.'" where id='.$_GET["id"].';';
+    mysqli_query($conn, $query);
+    mysqli_commit($conn);
+    $conn->close();
+    header("Location: /");
+} else {
+   edit($ip, $port, $login, $_GET['id']);
+}
+
+
+?>
+
+</body>
+
+</html>
---
+++ longpress.js
@@ -0,0 +1,66 @@
+var node = document.getElementsByTagName("tr");
+var longpress = false;
+var presstimer = null;
+var longtarget = null;
+
+var cancel = function(e) {
+    if (presstimer !== null) {
+        clearTimeout(presstimer);
+        presstimer = null;
+    }
+    
+    this.classList.remove("longpress");
+};
+
+var click = function(e) {
+    if (presstimer !== null) {
+        clearTimeout(presstimer);
+        presstimer = null;
+  var x = e.clientX, y = e.clientY,
+       elem = document.elementFromPoint(x, y);
+    if (elem.id) { 
+        window.location="edit.php?id="+elem.id;
+    }
+
+    }
+    
+    this.classList.remove("longpress");
+    
+    if (longpress) {
+        return false;
+    }
+};
+
+var start = function(e) {
+    console.log(e);
+    var x = e.clientX, y = e.clientY,
+       elem = document.elementFromPoint(x, y);
+    console.log("ID:"+elem.id);
+    if (e.type === "click" && e.button !== 0) {
+        return;
+    }
+    
+    longpress = false;
+    
+    this.classList.add("longpress");
+    
+    presstimer = setTimeout(function() {
+      var x = e.clientX, y = e.clientY,
+       elem = document.elementFromPoint(x, y);
+        longpress = true;
+        window.location="edit.php?id="+elem.id;
+    }, 2500);
+    
+    return false;
+};
+
+
+for (var i = 0; i < node.length; i++) {
+  node[i].addEventListener("mousedown", start);
+  node[i].addEventListener("touchstart", start);
+  node[i].addEventListener("click", click);
+  node[i].addEventListener("mouseout", cancel);
+  node[i].addEventListener("touchend", cancel);
+  node[i].addEventListener("touchleave", cancel);
+  node[i].addEventListener("touchcancel", cancel);
+}
